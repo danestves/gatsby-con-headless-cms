bbdc9c164ebfa7eb0c56de49bcd75547a550fee8
Daniel Esteves <daniel@seeed.us>
2021-04-25 22:10:49 -0400

    feat: header component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import * as React from "react"
 
+import Header from "./Header"
+
 const Layout = ({ children }) => {
   return (
     <>
-      <header>este es el header</header>
+      <Header />
 
       {children}
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,3 @@
 export { default as Layout } from "./Layout"
 export { default as Seo } from "./Seo"
+export { default as Header } from "./Header"
